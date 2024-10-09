df3fe26805058db52f5a918902da0ca5af1835c8
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-10-09 18:44:12 -0300

    Update atividade3.cs

--- Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade3.cs
+++ Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade3.cs
@@ -6,13 +6,10 @@ namespace Fatorial
     {
         static long CalcularFatorial(int numero)
         {
-            // Verifica se o número é negativo
             if (numero < 0)
             {
                 throw new ArgumentException("O número deve ser um inteiro positivo.");
             }
-
-            // Calcula o fatorial do número
             long fatorial = 1;
             for (int i = 1; i <= numero; i++)
             {
@@ -24,11 +21,8 @@ namespace Fatorial
 
         static void Main(string[] args)
         {
-            // Solicita um número ao usuário
             Console.Write("Digite um número: ");
             int numero = int.Parse(Console.ReadLine());
-
-            // Chama a função CalcularFatorial e exibe o fatorial
             long fatorial = CalcularFatorial(numero);
             Console.WriteLine("O fatorial de " + numero + " é: " + fatorial);
         }
