ada3241e5529233452d1e43a34493d93b7b1a5b2
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-10-09 18:43:21 -0300

    Update atividade1.cs

--- Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade1.cs
+++ Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade1.cs
@@ -6,14 +6,12 @@ namespace MediaAritmetica
     {
         static float CalcularMedia(float num1, float num2, float num3)
         {
-            // Calcula a média aritmética dos três números
             float media = (num1 + num2 + num3) / 3;
             return media;
         }
 
         static void Main(string[] args)
         {
-            // Solicita três números ao usuário
             Console.Write("Digite o primeiro número: ");
             float num1 = float.Parse(Console.ReadLine());
 
@@ -23,7 +21,6 @@ namespace MediaAritmetica
             Console.Write("Digite o terceiro número: ");
             float num3 = float.Parse(Console.ReadLine());
 
-            // Chama a função CalcularMedia e exibe a média
             float media = CalcularMedia(num1, num2, num3);
             Console.WriteLine("A média aritmética é: " + media);
         }
