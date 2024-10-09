f627931673625134a5af9835ca8e4dd0f4c0b423
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-10-09 18:44:33 -0300

    Update atividade4.cs

--- Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade4.cs
+++ Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade4.cs
@@ -6,18 +6,14 @@ namespace ConversaoDeTemperatura
     {
         static float ConverterParaFahrenheit(float celsius)
         {
-            // Converte a temperatura de Celsius para Fahrenheit
             float fahrenheit = (celsius * 9 / 5) + 32;
             return fahrenheit;
         }
 
         static void Main(string[] args)
         {
-            // Solicita uma temperatura em Celsius ao usuário
             Console.Write("Digite uma temperatura em Celsius: ");
             float celsius = float.Parse(Console.ReadLine());
-
-            // Chama a função ConverterParaFahrenheit e exibe o resultado
             float fahrenheit = ConverterParaFahrenheit(celsius);
             Console.WriteLine(celsius + " graus Celsius é igual a " + fahrenheit + " graus Fahrenheit");
         }
