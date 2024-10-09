d47bf406ff4fbebe8af90d4b5f9fbd9f449b336f
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-10-09 18:43:46 -0300

    Update atividade2.cs

--- Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade2.cs
+++ Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade2.cs
@@ -6,13 +6,10 @@ namespace NumeroPrimo
     {
         static string EhPrimo(int numero)
         {
-            // Verifica se o número é menor que 2
             if (numero < 2)
             {
                 return "Não!";
             }
-
-            // Verifica se o número é primo
             for (int i = 2; i * i <= numero; i++)
             {
                 if (numero % i == 0)
@@ -26,11 +23,9 @@ namespace NumeroPrimo
 
         static void Main(string[] args)
         {
-            // Solicita um número ao usuário
             Console.Write("Digite um número: ");
             int numero = int.Parse(Console.ReadLine());
 
-            // Chama a função EhPrimo e exibe se o número é primo
             string ehPrimo = EhPrimo(numero);
             Console.WriteLine("O número " + numero + " é primo? " + ehPrimo);
         }
