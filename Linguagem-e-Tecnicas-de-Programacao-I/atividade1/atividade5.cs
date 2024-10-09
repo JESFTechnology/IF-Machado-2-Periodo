309e9266e685dabaa8788fbb29f4549dc5c197f3
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-10-09 18:45:33 -0300

    Update atividade5.cs

--- Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade5.cs
+++ Linguagem-e-Tecnicas-de-Programacao-I/atividade1/atividade5.cs
@@ -6,21 +6,16 @@ namespace CalculadoraDePotencia
     {
         static double CalcularPotencia(int baseNumber, int exponent)
         {
-            // Calcula a potência da base elevada ao expoente
             double result = Math.Pow(baseNumber, exponent);
             return result;
         }
 
         static void Main(string[] args)
         {
-            // Solicita a base e o expoente ao usuário
             Console.Write("Digite a base: ");
             int baseNumber = int.Parse(Console.ReadLine());
-
             Console.Write("Digite o expoente: ");
             int exponent = int.Parse(Console.ReadLine());
-
-            // Chama a função CalcularPotencia e exibe o resultado
             double result = CalcularPotencia(baseNumber, exponent);
             Console.WriteLine(baseNumber + " elevado ao " + exponent + " é igual a " + result);
         }
