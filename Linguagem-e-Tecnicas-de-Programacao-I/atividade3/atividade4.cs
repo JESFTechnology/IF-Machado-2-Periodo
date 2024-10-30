70d25af7ca44a16a43f410a13d9ff4a6da312f44
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-10-30 15:21:44 -0300

    Update atividade4.cs

--- Linguagem-e-Tecnicas-de-Programacao-I/atividade3/atividade4.cs
+++ Linguagem-e-Tecnicas-de-Programacao-I/atividade3/atividade4.cs
@@ -1,1 +1,31 @@
-oi
+using System;
+
+class Program
+{
+    static int ContarI(int[] vetor)
+    {
+        int contador = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] % 2 != 0)
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine("Digite o número de elementos do vetor:");
+        int n = int.Parse(Console.ReadLine());
+        int[] vetor = new int[n];
+        Console.WriteLine("Digite "+n+" números inteiros:");
+        for (int i = 0; i < n; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+        int quantidadeImpares = ContarI(vetor);
+        Console.WriteLine("Quantidade de números ímpares no vetor: "+quantidadeImpares);
+    }
+}
